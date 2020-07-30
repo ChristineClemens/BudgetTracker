12fb71125c74e22b3e9edba4b8afdf48c0d16608
Christine Clemens <christineclemens.tesol@gmail.com>
2020-07-29 22:51:56 -0400

    Added urlsToCache variable in service-worker.js file.

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -21,6 +21,16 @@ const FILES_TO_CACHE = [];
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+var urlsToCache = [
+    "/",
+    "/db.js",
+    "/index/js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
 //Caching capability initialization
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
